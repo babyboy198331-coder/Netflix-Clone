ba07119f777878dfd5ad1026cb0dd43f7637a779
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-22 18:32:03 -0500

    master

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,13 +21,18 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="navbar-right"></div>
-      <img src={search_icon} alt="" className='icons' />
-      <p>Children</p>
-      <img src={bell_icon} alt="" className='icons' />
-      <div className="navbar-profile">
-         <img src={profile_img} alt="" className='profile' />
-            <img src={caret_icon} alt="" />
+      {/* FIXED HERE */}
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className='icons' />
+        <p>Children</p>
+        <img src={bell_icon} alt="" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className='profile' />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
+        </div>
       </div>
     </div>
   )
