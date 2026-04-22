a59fad6d2d5de261d77474738e5e1edd5f530ef3
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-22 18:38:04 -0500

    master

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -15,6 +17,10 @@ const Home = () => {
           <p>Discovering his ties to a secret ancient order, a young man 
             living in modern Istanbul embarks on a quest to save 
             the city from an immortal enemy.</p>
+            <div className="hero-btns">
+              <button><img src={play_icon} alt="" />Play</button>
+              <button><img src={info_icon} alt="" />Play</button>
+            </div>
         </div>
        </div>
     </div>
