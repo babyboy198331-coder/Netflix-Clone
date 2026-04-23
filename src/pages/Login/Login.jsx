b71b8a50c685dac89e145120a852fa282ec7ea1b
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-23 14:07:40 -0500

    master

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,23 +5,33 @@ import logo from '../../assets/logo.png'
 const Login = () => {
   return (
     <div className='login'>
-<img src={logo} className='login-logo' alt=""/>
-<div className="login-form">
-  <h1>Sign Up</h1>
-  <form>
-    <input type="text" placeholder='Your name'/>
-    <input type="email" placeholder='Email'/>
-    <input type="password" placeholder='Password'/>
-    <button>Sign Up</button>
-    <div className="form-help">
-      <div className="remember">
-        <input type="checkbox"/>
-        <label htmlFor="Remeber Me"></label>
-      </div>
-      <p>Need Help?</p>
-    </div>
-  </form>
+      
+      <img src={logo} className='login-logo' alt="Logo" />
+
+      <div className="login-form">
+        <h1>Sign Up</h1>
+
+        <form>
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+
+          <button type="submit">Sign Up</button>
+
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remember">Remember me</label>
+            </div>
+
+            <p>Need Help?</p>
+          </div>
+        </form>
+<div className="form-switch">
+  <p>New to Netflix? <span>Sign Up Now</span></p>
+  <p>Already have an Account? <span>Sign In Now</span></p>
 </div>
+      </div>
     </div>
   )
 }
