b207bf8acbf08e2493690b8b7fb18c6d94effd8c
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-23 16:05:40 -0500

    master

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -9,56 +9,60 @@ const TitleCards = ({ title, category }) => {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1M2RkMjUyZmUxZjc0ZDdkNmY4ZDk5NGYzYzg4MzQwMCIsIm5iZiI6MTc3Njg3Nzk2OC4wOCwic3ViIjoiNjllOTAxOTBkNWYzZTk3YTQzMDk3ZjdiIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.RMY2TgQdU6e_hze1rC-j3dIMh05Coaz_CA4SHQ-R57o'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1M2RkMjUyZmUxZjc0ZDdkNmY4ZDk5NGYzYzg4MzQwMCIsIm5iZiI6MTc3Njg3Nzk2OC4wOCwic3ViIjoiNjllOTAxOTBkNWYzZTk3YTQzMDk3ZjdiIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.RMY2TgQdU6e_hze1rC-j3dIMh05Coaz_CA4SHQ-R57o'
     }
   }
 
-  const handleWheel = (event) => {
-    if (!cardsRef.current) return
-    event.preventDefault()
-    cardsRef.current.scrollLeft += event.deltaY
-  }
-
   useEffect(() => {
-    fetch(
-      'https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1',
-      options
-    )
-      .then((res) => res.json())
-      .then((res) => setApiData(res?.results || []))
-      .catch((err) => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
+          options
+        )
+        const data = await res.json()
+        setApiData(data?.results || [])
+      } catch (err) {
         console.error(err)
         setApiData([])
-      })
+      }
+    }
 
-    const element = cardsRef.current
-    if (!element) return
+    fetchData()
+  }, [category])
 
-    element.addEventListener('wheel', handleWheel, { passive: false })
+  useEffect(() => {
+    const el = cardsRef.current
+    if (!el) return
+
+    const handleWheel = (e) => {
+      e.preventDefault()
+      el.scrollLeft += e.deltaY
+    }
+
+    el.addEventListener('wheel', handleWheel, { passive: false })
 
     return () => {
-      element.removeEventListener('wheel', handleWheel)
+      el.removeEventListener('wheel', handleWheel)
     }
   }, [])
 
   return (
     <div className="title-cards">
-      <h2>{title || 'Popular on Netflix'}</h2>
+      <h2>{title}</h2>
 
       <div className="card-list" ref={cardsRef}>
-        {(apiData || []).map((card) => {
-          const img = card?.backdrop_path
-            ? `https://image.tmdb.org/t/p/w500${card.backdrop_path}`
-            : null
-
-          return (
-            <div className="card" key={card?.id}>
-              {img && <img src={img} alt={card?.title || 'movie'} />}
-              <p>{card?.original_title}</p>
-            </div>
-          )
-        })}
+        {apiData.map((movie) => (
+          <div className="card" key={movie.id}>
+            {movie.backdrop_path && (
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt={movie.original_title}
+              />
+            )}
+            <p>{movie.original_title}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
