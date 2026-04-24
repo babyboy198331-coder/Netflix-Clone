44c507d810d1d99c15a9e2464e2931258e64388f
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-24 12:56:45 -0500

    master

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -4,10 +4,8 @@ import back_arrow_icon from '../../assets/back_arrow_icon.png'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
-
-  const {id} = useParams()
-const navigate = useNavigate();
-
+  const { id } = useParams()
+  const navigate = useNavigate()
 
   const [apiData, setApiData] = useState({
     name: "",
@@ -20,36 +18,46 @@ const navigate = useNavigate();
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1M2RkMjUyZmUxZjc0ZDdkNmY4ZDk5NGYzYzg4MzQwMCIsIm5iZiI6MTc3Njg3Nzk2OC4wOCwic3ViIjoiNjllOTAxOTBkNWYzZTk3YTQzMDk3ZjdiIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.RMY2TgQdU6e_hze1rC-j3dIMh05Coaz_CA4SHQ-R57o'
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1M2RkMjUyZmUxZjc0ZDdkNmY4ZDk5NGYzYzg4MzQwMCIsIm5iZiI6MTc3Njg3Nzk2OC4wOCwic3ViIjoiNjllOTAxOTBkNWYzZTk3YTQzMDk3ZjdiIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.RMY2TgQdU6e_hze1rC-j3dIMh05Coaz_CA4SHQ-R57o'
     }
   }
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
       .then(res => res.json())
       .then(data => {
-        if (data.results && data.results.length > 0) {
+        if (data.results?.length > 0) {
           setApiData(data.results[0])
         }
       })
       .catch(err => console.error(err))
-  }, [])
+  }, [id])
 
   return (
     <div className='player'>
-      <img src={back_arrow_icon} alt="back" onClick={() => {navigate(-2)}} />
+      <img
+        src={back_arrow_icon}
+        alt="back"
+        onClick={() => navigate(-1)}
+      />
 
-      <iframe
-        width='90%'
-        height='90%'
-        src={`https://www.youtube.com/embed/${apiData.key}`}
-        title='trailer'
-        frameBorder='0'
-        allowFullScreen
-      ></iframe>
+      {apiData.key && (
+        <iframe
+          width='90%'
+          height='90%'
+          src={`https://www.youtube.com/embed/${apiData.key}`}
+          title='trailer'
+          frameBorder='0'
+          allowFullScreen
+        />
+      )}
 
       <div className="player-info">
-        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.published_at ? apiData.published_at.slice(0, 10) : ""}</p>
         <p>{apiData.name}</p>
         <p>{apiData.type}</p>
       </div>
