69e0e66f9e0118ce8727745afad929273727b560
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-24 17:44:03 -0500

    master

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,14 +12,24 @@ const Navbar = () => {
   const navRef = useRef();
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if(window.scrollY >= 80){
+
+    const handleScroll = () => {
+      if (!navRef.current) return;
+
+      if (window.scrollY >= 80) {
         navRef.current.classList.add('nav-dark')
-      }else{
-         navRef.current.classList.remove('nav-dark')
+      } else {
+        navRef.current.classList.remove('nav-dark')
       }
-    })
-  }, [])
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+
+  }, []);
 
   return (
     <div ref={navRef} className='navbar'>
@@ -35,7 +45,6 @@ const Navbar = () => {
         </ul>
       </div>
 
-      
       <div className="navbar-right">
         <img src={search_icon} alt="" className='icons' />
         <p>Children</p>
@@ -44,7 +53,7 @@ const Navbar = () => {
           <img src={profile_img} alt="" className='profile' />
           <img src={caret_icon} alt="" />
           <div className="dropdown">
-            <p onClick={() => {logout()}}>Sign Out of Netflix</p>
+            <p onClick={() => logout()}>Sign Out of Netflix</p>
           </div>
         </div>
       </div>
