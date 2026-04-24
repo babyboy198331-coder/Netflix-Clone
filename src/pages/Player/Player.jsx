e3e22e4075c699a26948a7e8f4713608fa004cd4
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-24 11:30:46 -0500

    master

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
-
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width='90%'
+        height='90%'
+        src='https://youtube.com/embed/YQQD67N5pi0'
+        title='trailer' frameBorder='0' allowFullScreen></iframe>
+        <div className="player-info">
+          <p>Published Date</p>
+          <p>Name</p>
+          <p>Type</p>
+        </div>
     </div>
   )
 }
 
-export default className='player'
+export default Player
